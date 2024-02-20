f5c3b9c6d3a07a5c1aeaee075100dff3dff9af1c
0xHemaSallam <hemasallam03@gmail.com>
2024-02-21 01:42:22 +0200

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, int size)
+{
+int i, j;
+int tmp;
+if (size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j] > array[j + 1])
+{
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+print_array(array, size);
+}
+}
+}
